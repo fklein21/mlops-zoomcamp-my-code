ca475f8e836df657bd52481929b01d90db542bc9
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-07-21 00:02:42 +0200

    week 5

---
+++ week5-monitoring/prediction-service/app.py
@@ -0,0 +1,55 @@
+import os
+import pickle
+import requests
+
+from pymongo import MongoClient
+
+from flask import Flask, request, jsonify
+
+MODEL_FILE = os.getenv('MODEL_FILE', 'lin_reg.bin')
+MONGODB_ADDRESS = os.getenv('MONGODB_ADDRESS', 'mongodb://127.0.0.1:27017')
+EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE_ADDRESS', 'http://127.0.0.1:5000')
+
+with open(MODEL_FILE, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+app = Flask('duration')
+mongo_client = MongoClient(MONGODB_ADDRESS)
+db = mongo_client.get_database('prediction_service')
+collection = db.get_collection('data')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    record = request.get_json()
+    record['PU_DO'] = '%s_%s' % (record['PULocationID'], record['DOLocationID'])
+    X = dv.transform([record])
+    prediction = model.predict(X)
+
+    result = {
+        'duration': float(prediction)
+    }
+
+    save_to_db(record, prediction)
+    send_to_evidently_service(record, prediction)
+
+    return jsonify(result)
+
+
+def save_to_db(record, prediction):
+    rec = record.copy()
+    rec['prediction'] = prediction
+    collection.insert_one(rec)
+    pass
+
+
+def send_to_evidently_service(record, prediction):
+    rec = record.copy()
+    rec['prediction'] = prediction
+    requests.post(f'{EVIDENTLY_SERVICE_ADDRESS}/iterate/taxi', json=[rec])
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
+
